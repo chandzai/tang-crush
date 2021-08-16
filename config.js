4b1987d9247926efe81d5edc8cf223915f3c8676
Junno <70685619+chandzai@users.noreply.github.com>
2021-08-16 20:24:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'iem iu à!',
+    introDesc: `Cậu dễ thương như vậy, bị gió kia thổi đến vòng tay của tớ rồi đó, tớ sẽ không trả lại đâu😙<3.`,
+    btnIntro: 'Chúng ta chỉ là bạn <333333',
+    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích cậu <33',
     btnNo: 'Hông,đâu !:3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Yêu cậu nhìu nhìu vì cậu là 1 phần cuộc sống của tớ <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Iu công chúa của tớ.',
+    btnAccept: 'ò ò lun <3',
     messLink: 'https://www.facebook.com/messages/t/100057609429373/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
